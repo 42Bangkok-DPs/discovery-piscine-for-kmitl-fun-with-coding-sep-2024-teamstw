eeda82d714157e58d2025fdf1a9f4ea319a8bead
teamstw <116998615+teamstw@users.noreply.github.com>
2024-09-14 15:16:53 +0700

    Add files via upload

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,11 @@
 if [ $# -eq 0 ]; then
     echo "No arguments provided."
 elif [ $# -gt 3 ]; then
-    echo "Arguments: $1 $2 $3"
+    echo $1  
+    echo $2
+    echo $3
 else
-    echo "Arguments: $1 $2 $3"
+    echo $1  
+    echo $2
+    echo $3
 fi
