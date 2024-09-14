718de02f70f0ca20201f9bace96548a743349996
teamstw <116998615+teamstw@users.noreply.github.com>
2024-09-14 15:02:57 +0700

    Add files via upload

---
+++ cell00/ex05/build.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]; then
+  echo "No arguments provided. Please provide folder names."
+  exit 1
+fi
+
+for folder in "$@"
+do
+  new_folder="ex${folder}"
+
+  mkdir -p "$new_folder"
+  echo "Created folder: $new_folder"
+done
